eeb467a718c20cf9dcc9a1235518e106beda45d7
herphay <herphay@gmail.com>
2025-03-30 17:48:46 +0800

    Commit app.py to initial functionality

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print('hello, world')
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/apv")
+def apv():
+    return render_template("apv.html")
+
+@app.route("/tables")
+def tables():
+    return render_template("tables.html")
+
+@app.route("/compare")
+def compare():
+    return render_template("compare.html")
