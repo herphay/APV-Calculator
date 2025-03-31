7ed28a39fb15f2d76843bae9c0c12be7930bae7c
herphay <herphay@gmail.com>
2025-03-31 12:25:55 +0800

    Added lifetables.db and lifetables.csv for the database and raw data

--- helpers.py
+++ helpers.py
@@ -1,5 +1,16 @@
 import csv
+import sqlite3
 
 def main():
-    with open('lifetable.csv', mode='r') as file:
+    """
+    Main function is used to create a table for lifetable data and insert them into the database.
+    Pre-requisit:
+        - lifetables sqlite3 database to be created
+        - lifetable data to be stored in a csv file named lifetables
+    """
+    with open('lifetables.csv', mode='r') as file:
         reader = csv.DictReader(file)
+
+
+if __name__ == '__main__':
+    main()
