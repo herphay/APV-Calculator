e0bb014a3d5a2f4d96ad5be77a8c2ffd35651773
herphay <herphay@gmail.com>
2025-03-31 12:02:48 +0800

    Added lifetable explanation & starting of database creation

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 @app.route("/")
 def index():
     return render_template("index.html")
